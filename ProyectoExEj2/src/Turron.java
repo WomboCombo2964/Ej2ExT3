ad2464f0f33b82fe29303f47dd8e98ede69f1097
WomboCombo2964 <quiquediaz.cas@gmail.com>
2023-12-01 19:46:19 +0100

    Mod clase turron

--- ProyectoExEj2/src/Turron.java
+++ ProyectoExEj2/src/Turron.java
@@ -3,6 +3,7 @@ public class Turron {
 
 	public static void main(String[] args) {
 		System.out.println("Soy Enrique y me encantan los turrones.");
+		System.out.println("Pero mi preferido es el de chocolate");
 
 	}
 
